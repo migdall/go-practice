4aa2e3e60312aa80320fb1480c701a6a41a9fa28
Jesus Noland <jn@jesusnoland.com>
2019-07-05 14:27:14 -0700

    Adding print statements for exercise 1

--- exercise1/exercise1.go
+++ exercise1/exercise1.go
@@ -9,6 +9,10 @@ func main() {
 	y := "James Bond"
 	z := true
 
+	// Now print out these variables on a single
+	// line and multiple lines
+	fmt.Println(x, y, z)
+
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
